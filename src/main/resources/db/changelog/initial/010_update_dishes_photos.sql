b23bee8481d316270eee2a52d03142821a268c12
Ivanov Maksat <zer0icemax@gmail.com>
2025-05-01 03:24:35 +0600

    updated photos for dishes (#3)
    Co-authored-by: Aruush <aruuke.toktosunovaa@gmail.com>

---
+++ src/main/resources/db/changelog/initial/010_update_dishes_photos.sql
@@ -0,0 +1,100 @@
+update dishes set photo_url = 'https://static.1000.menu/img/content-v2/ef/27/10853/picca-margarita-v-domashnix-usloviyax_1608783820_4_max.jpg'
+where restaurants_id = 6 and name = 'Пицца Маргарита';
+update dishes set photo_url = 'https://okeansushi.ru/templates/sushi/blog/pitstsa-pepperoni-istoriya-sostav-retsept-4.jpg'
+where restaurants_id = 6 and name = 'Пицца Пепперони';
+update dishes set photo_url = 'https://leogrillpizza.ru/wp-content/uploads/2025/02/25ae0d99-4fda-45df-9acc-615cbd47cdd6.jpg'
+where restaurants_id = 6 and name = 'Пицца Четыре сыра';
+update dishes set photo_url = 'https://images.gastronom.ru/iWgv0SDZcskl1N6evw7QgvfTrcMupWv32A-gDVE1eLw/pr:article-cover-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzLzJjYWY0MGJmLWRhOTctNDZhMi04MjZiLTFkMTc0ZjZmZjU4Yi5qcGc.webp'
+where restaurants_id = 6 and name = 'Пицца Гавайская';
+update dishes set photo_url = 'https://papitospizza.ru/wa-data/public/shop/products/39/00/39/images/278/278.750x0.jpg'
+where restaurants_id = 6 and name = 'Пицца Карбонара';
+update dishes set photo_url = 'https://pizzaq.ru/upload/resize_cache/iblock/e63/600_600_1/e635c3850111bea7649596e11fba742a.jpg'
+where restaurants_id = 6 and name = 'Пицца Диабло';
+update dishes set photo_url = 'https://mychefpizza.ru/wp-content/uploads/2016/02/Diablo_2.jpg'
+where restaurants_id = 6 and name = 'Кальцоне';
+update dishes set photo_url = 'https://img.povar.ru/mobile/1c/35/7d/b9/salat_s_semgoi_mocarelloi_i_rukkoloi-782716.jpg'
+where restaurants_id = 6 and name = 'Салат с моцареллой';
+update dishes set photo_url = 'https://cdn.vkuso.ru/uploads/syrno-chesnochnye-palochki-v-duxovke-914-640x440.jpg'
+where restaurants_id = 6 and name = 'Чесночные палочки';
+update dishes set photo_url = 'https://s0.rbk.ru/v6_top_pics/media/img/9/42/347424804589429.webp'
+where restaurants_id = 6 and name = 'Тирамису';
+update dishes set photo_url = 'https://images.gastronom.ru/DO5E0rfFUbWJiTN8C7dN1-vxhl48TA72FoojwwGUR9Y/pr:article-cover-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzL2U0NGZkZDczLWFjYWQtNGIxZi05ZWU1LTlkMzBkOTc5Y2VkNy5qcGc.webp'
+where restaurants_id = 7 and name = 'Рибай стейк';
+update dishes set photo_url = 'https://media.istockphoto.com/id/453169607/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B3%D0%BE%D0%B2%D1%8F%D0%B4%D0%B8%D0%BD%D0%B0-%D1%81%D1%82%D0%B5%D0%B9%D0%BA.jpg?s=612x612&w=0&k=20&c=kMIJUF3tmNgiEvNU-1M8BNzUIeJehnuMpx8MTaWvH1Q='
+where restaurants_id = 7 and name = 'Филе-миньон';
+update dishes set photo_url = 'https://chefrestoran.ru/wp-content/uploads/2021/11/tomagav.jpg'
+where restaurants_id = 7 and name = 'Томагавк стейк';
+update dishes set photo_url = 'https://www.man-meat.ru/upload/iblock/f9f/farsh1.jpg'
+where restaurants_id = 7 and name = 'Бургер из мраморной говядины';
+update dishes set photo_url = 'https://images.gastronom.ru/-_y40_a39ngrysDjx5XnDWHadWbGLbncxaYWwei9Mc8/pr:recipe-cover-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzLzgzZGI3NmI5LTllZGYtNDk3Ny1hNTdmLTE3MjBjOTlhMDYzMS5qcGc.webp'
+where restaurants_id = 7 and name = 'Запеченый картофель';
+update dishes set photo_url = 'https://grandkulinar.ru/uploads/posts/2019-05/1558802912_sparzha-zavyornutaya-v-bekon-na-grile.jpg'
+where restaurants_id = 7 and name = 'Спаржа на гриле';
+update dishes set photo_url = 'https://s0.rbk.ru/v6_top_pics/media/img/5/11/346965338140115.webp'
+where restaurants_id = 7 and name = 'Фуа-гра';
+update dishes set photo_url = 'https://static.1000.menu/img/content/33135/korolevskie-krevetki-na-grile_1551983821_11_max.jpg'
+where restaurants_id = 7 and name = 'Креветки гриль';
+update dishes set photo_url = 'https://cooking-img.nv.ua/cooking/recipe/8009/TEtJIPgFVC.webp?w=778'
+where restaurants_id = 7 and name = 'Салат "Цезарь" с креветками';
+update dishes set photo_url = 'https://art-lunch.ru/content/uploads/2014/08/cheesecake-new-york-001x2-1.jpg'
+where restaurants_id = 7 and name = 'Чизкейк Нью-Йорк';
+update dishes set photo_url = 'https://volshebnaya-eda.ru/wp-content/uploads/2015/05/zharkoe-po-domashnemu.jpg'
+where restaurants_id = 8 and name = 'Жаркое по-домашнему';
+update dishes set photo_url = 'https://navat.kg/dostavka/files/products/kurinaya-grudka-pp-1.800x600w.jpg'
+where restaurants_id = 8 and name = 'Куриная грудка в сливочном соусе';
+update dishes set photo_url = 'https://i.ytimg.com/vi/8kQB7xrstBw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDebh_Q_P-EC0VSym4pi-KoiQFu9w'
+where restaurants_id = 8 and name = 'Греческий салат';
+update dishes set photo_url = 'https://fort.crimea.com/catering/uploads/fotos/1c31367d-8c1f-11e9-9c70-54a05051519a_1.jpg'
+where restaurants_id = 8 and name = 'Суп-пюре из шампиньонов';
+update dishes set photo_url = 'https://img.iamcook.ru/2023/upl/recipes/cat/u-ac528b2483c75d2dbe1dcb41762d26e9.jpg'
+where restaurants_id = 8 and name = 'Картофельные драники';
+update dishes set photo_url = 'https://images.unian.net/photos/2020_07/thumb_files/1200_0_1594844831-9010.jpg'
+where restaurants_id = 8 and name = 'Голубцы';
+update dishes set photo_url = 'https://a-furshet.ru/mag/photo/norm131.jpg'
+where restaurants_id = 8 and name = 'Сырная тарелка';
+update dishes set photo_url = 'https://images.gastronom.ru/KQrCvjBr1In5v-YuzV5KeQrXRze7i1U4JxtPhXGHUOA/pr:article-preview-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzL2E2MDI3Yzg0LTQ1MDUtNDg2YS05MzdkLWJjMzQxNGI3ZjFmZi5qcGc'
+where restaurants_id = 8 and name = 'Шарлотка с яблоками';
+update dishes set photo_url = 'https://img.iamcook.ru/2017/upl/recipes/cat/u6009-d578d08d04e059a9ccb4729943b5d3d8.jpg'
+where restaurants_id = 8 and name = 'Винегрет';
+update dishes set photo_url = 'https://static.1000.menu/img/content-v2/64/20/4903/kompot-iz-suhofruktov_1590812179_14_max.jpg'
+where restaurants_id = 8 and name = 'Компот из сухофруктов';
+update dishes set photo_url = 'https://soluxeclub.com/kp/wp-content/uploads/2020/08/pekinskaya-utka.jpg'
+where restaurants_id = 9 and name = 'Утка по-пекински';
+update dishes set photo_url = 'https://24shashlik.ru/assets/images/products/94/imgp6197.jpg'
+where restaurants_id = 9 and name = 'Кисло-сладкая свинина';
+update dishes set photo_url = 'https://cdn.lifehacker.ru/wp-content/uploads/2021/05/shutterstock_1174099135_1621521727-1280x640.jpg'
+where restaurants_id = 9 and name = 'Курица генерала Цо';
+update dishes set photo_url = 'https://telegraf.com.ua/static/storage/originals/8/ab/a120b24ef90ce6581e2b48c9c72efab8.jpg'
+where restaurants_id = 9 and name = 'Вонтоны жареные';
+update dishes set photo_url = 'https://www.patee.ru/r/x6/19/7a/dc/960m.jpg'
+where restaurants_id = 9 and name = 'Суп том ям';
+update dishes set photo_url = 'https://volshebnaya-eda.ru/wp-content/uploads/2024/10/lapsha-udon-s-kuricej-i-ovoshhami-v-souse-teriyaki-12.jpg'
+where restaurants_id = 9 and name = 'Лапша удон с овощами';
+update dishes set photo_url = 'https://img.povar.ru/mobile/7d/68/32/62/salat_s_hrustyashimi_baklajanami_i_sladkim_sousom_chili-791360.jpg'
+where restaurants_id = 9 and name = 'Баклажаны в соусе чили';
+update dishes set photo_url = 'https://kingikra.ru/upload/ammina.optimizer/jpg-webp/q75/upload/iblock/045/idzr7rdoomoxd00wsticu9hm08s82nbt.webp'
+where restaurants_id = 9 and name = 'Жареный рис с морепродуктами';
+update dishes set photo_url = 'https://www.kikkoman.ru/fileadmin/_processed_/e/b/csm_143-recipe-page-Fresh-Spring-Rolls-with-Teriyaki-Aubergine_desktop_8f13a454b2.jpg'
+where restaurants_id = 9 and name = 'Спринг-роллы';
+update dishes set photo_url = 'https://images.gastronom.ru/0kD_pIY0ADF-dKiYYJkC2KwKT2wN67tbKPMfaA8hqvY/pr:content-group-preview-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzLzgyZmMyMzZkLTJkYzUtNDVlZS1hNWVhLTdmZjQ1YjdjYzY4ZC5qcGc.webp'
+where restaurants_id = 9 and name = 'Мороженое с жареным бананом';
+update dishes set photo_url = 'https://images.gastronom.ru/3livC6pk7uUsV9x-N0biacukvkpibSY5xilc0GDUsSE/pr:recipe-cover-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzLzczOGZjZTg1LTc2YmUtNGE1ZS05YjJiLWYzMzJhZDY0NzI0Mi5qcGc.webp'
+where restaurants_id = 10 and name = 'Круассан с ветчиной и сыром';
+update dishes set photo_url = 'https://cooklikemary.ru/sites/default/files/img_0001.jpg'
+where restaurants_id = 10 and name = 'Салат Нисуаз';
+update dishes set photo_url = 'https://cdn1.ozonusercontent.com/s3/club-storage/images/recipe_step/705/f73a33bc-916c-4311-adf6-4063b2007540.jpeg'
+where restaurants_id = 10 and name = 'Луковый суп';
+update dishes set photo_url = 'https://pudov.ru/upload/iblock/d68/k4ibcyyo601rvwft39kv90fdgl6izw6e.png'
+where restaurants_id = 10 and name = 'Киш Лорен';
+update dishes set photo_url = 'https://cdn.food.ru/unsigned/fit/640/480/ce/0/czM6Ly9tZWRpYS9waWN0dXJlcy9yZWNpcGVzLzEwMTUwOS9jb3ZlcnMvMzNYaXR4LmpwZWc.jpg'
+where restaurants_id = 10 and name = 'Багет с паштетом';
+update dishes set photo_url = 'https://cdn.lifehacker.ru/wp-content/uploads/2021/02/PHT_4034_1738526377.jpg'
+where restaurants_id = 10 and name = 'Тар-тар из говядины';
+update dishes set photo_url = 'https://zira.uz/wp-content/uploads/2022/05/klassicheskiy-ratatuy.jpg'
+where restaurants_id = 10 and name = 'Рататуй';
+update dishes set photo_url = 'https://kitchen-profi.ru/image/data/fondu/2.jpg'
+where restaurants_id = 10 and name = 'Фондю сырное';
+update dishes set photo_url = 'https://www.patee.ru/r/x6/0f/08/f6/960x.jpg'
+where restaurants_id = 10 and name = 'Профитроли';
+update dishes set photo_url = 'https://online-alina.ru/sites/online-alina.ru/files/s1200_3.jpg'
+where restaurants_id = 10 and name = 'Кафе глассе';
---
+++ src/main/resources/db/changelog/initial/011_update_photos_dishes.sql
@@ -0,0 +1,6 @@
+update dishes set photo_url = 'https://user36270.clients-cdnnow.ru/1672406138458-500x348.jpeg'
+where restaurants_id = 9 and name = 'Лапша удон с овощами';
+update dishes set photo_url = 'https://www.chefmarket.ru/blog/wp-content/uploads/2019/01/pots-of-meat-potatoes-e1548616326740.jpg'
+where restaurants_id = 8 and name = 'Жаркое по-домашнему';
+update dishes set photo_url = 'https://foodies.academy/wp-content/uploads/2020/05/franks_redhot_buffalo_chicken_wings_043-601x601.jpg'
+where restaurants_id = 2 and name = 'Куриные крылышки Buffalo';
