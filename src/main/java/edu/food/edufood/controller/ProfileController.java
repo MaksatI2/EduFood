503c3eb3c7ef68eb397415c5cdbda0e862d39205
I.Maksat <maksat@192.168.1.5>
2025-04-30 11:57:16 +0600

    Added profile and logo

---
+++ src/main/java/edu/food/edufood/controller/ProfileController.java
@@ -0,0 +1,29 @@
+package edu.food.edufood.controller;
+
+import edu.food.edufood.model.User;
+import edu.food.edufood.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String showProfile(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        String email = userDetails.getUsername();
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+
+        model.addAttribute("user", user);
+        return "user/profile";
+    }
+}
