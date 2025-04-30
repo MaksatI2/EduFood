b7d03508dcdfbb3ec8a7288e9584c8f00e352dfc
I.Maksat <maksat@192.168.1.5>
2025-05-01 02:56:25 +0600

    minor changes

--- src/main/java/edu/food/edufood/dto/OrderDTO.java
+++ src/main/java/edu/food/edufood/dto/OrderDTO.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -14,8 +13,6 @@ import java.util.List;
 public class OrderDTO {
     private Long id;
     private Long userId;
-    private Long restaurantId;
     private BigDecimal totalPrice;
     private LocalDateTime createdAt;
-    private List<OrderDishDTO> orderDishes;
 }
--- src/main/java/edu/food/edufood/service/impl/CustomUserDetailsService.java
+++ src/main/java/edu/food/edufood/service/impl/CustomUserDetailsService.java
@@ -1,10 +1,11 @@
 package edu.food.edufood.service.impl;
 
-import edu.food.edufood.repository.UserRepository;
 import edu.food.edufood.model.User;
+import edu.food.edufood.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.*;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,19 +17,17 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден: " + email));
 
         String role = user.getAccountType().name();
 
-        return new org.springframework.security.core.userdetails.User(
+        return new CustomUserDetails(
+                user.getId(),
                 user.getEmail(),
                 user.getPassword(),
                 user.getEnabled(),
-                true,
-                true,
-                true,
                 List.of(new SimpleGrantedAuthority(role))
         );
     }
