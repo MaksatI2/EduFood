c4f10a5dfc1041afd9a94e7c2646b53f1f7297a6
Ivanov Maksat <zer0icemax@gmail.com>
2025-05-02 14:55:40 +0600

    Feat/pagination dishes (#8)
    * updated photos for dishes
    
    * pagination for dishes
    
    * pagination color for dishes
    
    ---------
    
    Co-authored-by: Aruush <aruuke.toktosunovaa@gmail.com>

--- src/main/java/edu/food/edufood/repository/DishesRepository.java
+++ src/main/java/edu/food/edufood/repository/DishesRepository.java
@@ -1,6 +1,8 @@
 package edu.food.edufood.repository;
 
 import edu.food.edufood.model.Dishes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.List;
 @Repository
 public interface DishesRepository extends JpaRepository<Dishes, Long> {
     List<Dishes> findByRestaurantId(Long restaurantId);
+    Page<Dishes> findAll(Pageable pageable);
 }
--- src/main/java/edu/food/edufood/service/DishesService.java
+++ src/main/java/edu/food/edufood/service/DishesService.java
@@ -1,14 +1,15 @@
 package edu.food.edufood.service;
 
 import edu.food.edufood.dto.DishesDTO;
-import edu.food.edufood.model.Dishes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Map;
 
 public interface DishesService {
+    Page<DishesDTO> getAllDishes(Pageable pageable);
     Dishes getDishById(Long dishId);
-    List<DishesDTO> getAllDishes();
     List<DishesDTO> getDishesByRestaurantId(Long restaurantId);
     List<DishesDTO> getDishesByIds(List<Long> dishIds);
     Map<Long, Dishes> getDishesMapByIds(List<Long> ids);
