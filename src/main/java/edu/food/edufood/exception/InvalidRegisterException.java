8d4bce1d861a6d68f24b7d0194410d04c3c6e1e7
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-05-02 23:38:38 +0600

    Added InvalidRegisterException

---
+++ src/main/java/edu/food/edufood/exception/InvalidRegisterException.java
@@ -0,0 +1,23 @@
+package edu.food.edufood.exception;
+
+import java.util.NoSuchElementException;
+
+public class InvalidRegisterException extends NoSuchElementException {
+    private final String fieldName;
+    private final String message;
+
+    public InvalidRegisterException(String fieldName, String message) {
+        super(message);
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
